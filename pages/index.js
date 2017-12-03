e3d3ec9315dfffc94a683f128de46b954e3b5778
Roto <roto@smartstudy.co.kr>
2017-12-03 17:05:36 +0900

    og:image:width 및 og:image:height 추가

--- pages/index.js
+++ pages/index.js
@@ -18,6 +18,8 @@ const IndexPage = () => (
       <meta property="og:url" content={url} />
       <meta property="og:description" content={description} />
       <meta property="og:image" content={image} />
+      <meta property="og:image:width" content="1147" />
+      <meta property="og:image:height" content="1147" />
       <meta name="twitter:card" value="summary_large_image" />
       <meta name="twitter:site" content="@winterwolf0412" />
       <meta name="twitter:title" content={title} />
