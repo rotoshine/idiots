3fb0593ee734d604f516c33939be152a483784f5
rotoshine <rotoshine@gmail.com>
2019-12-01 00:58:15 +0900

    contact 수정

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,24 +1,24 @@
-import React from 'react'
+import "./contact.scss"
 
-import Layout from '../components/Layout'
-import Meta from '../components/Meta'
-import Container from '../components/Container'
-import Panel from '../components/Panel'
+import React from "react";
 
-import './contact.scss'
-
-export default () => (
+import Layout from "../components/Layout";
+import Meta from "../components/Meta";
+import Container from "../components/Container";
+import Panel from "../components/Panel";
+ 
+export default () => (   
   <Layout className="Contact">
     <Meta
       title="이디어츠(Idiots) 섭외 문의"
-      description="리더 최효근의 메일이나 이디어츠의 SNS를 통해 연락주세요."
+      description="yeonrock@idiots.mail 이나 이디어츠의 각종 SNS를 통해 연락주세요. 섭외문의 기다립니다!!!"
     />
     <Container>
       <Panel title="홈페이지 관련 문의">
         <ul>
           <li>
             <a href="https://twitter.com/winterwolf0412" target="_blank">
-              <i className="icon ion-logo-twitter"/> @winterwolf0412
+              <i className="icon ion-logo-twitter" /> @winterwolf0412
             </a>
           </li>
           <li>
@@ -34,23 +34,30 @@ export default () => (
         <ul>
           <li>
             <a href="https://www.facebook.com/bandidiots/" target="_blank">
-              <i className="icon ion-logo-facebook"/> https://www.facebook.com/bandidiots/
+              <i className="icon ion-logo-facebook" /> Facebook</a>
+          </li>
+          <li>
+            <a href="https://www.instagram.com/idiots.band" target="_blank">
+              <i className="icon ion-logo-instagram" /> Instagram</i>
             </a>
           </li>
           <li>
+            <a href="https://twitter.com/bullwoostar" target="_blank">
+              <i className="icon ion-logo-twitter" /> @bullwoostar
+            </a>
           </li>
           <li>
-            <a href="https://twitter.com/bullwoostar" target="_blank">
-              <i className="icon ion-logo-twitter"/> @bullwoostar
+            <a href="mailto:yeonrock@idiots.band">
+              <i className="icon ion-ios-mail" /> yeonrock@idiots.band
             </a>
           </li>
           <li>
             <a href="mailto:ccatal66@naver.com">
               <i className="icon ion-ios-mail" /> ccatal66@naver.com
-              </a>
+            </a>
           </li>
         </ul>
       </Panel>
     </Container>
   </Layout>
-)
+);
