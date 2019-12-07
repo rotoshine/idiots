24a6c3db31343c9cf6851b0ca9bd3e9428d7d86a
rotoshine <rotoshine@gmail.com>
2019-12-07 17:36:40 +0900

    공연 포스터 아래에 margin 추가

--- src/components/AlbumDetail.tsx
+++ src/components/AlbumDetail.tsx
@@ -1,9 +1,9 @@
 import { graphql, Link } from 'gatsby'
 import React, { ReactNode } from 'react'
-import Layout from './Layout';
-import Panel from './Panel';
+import Layout from './Layout'
+import Panel from './Panel'
 import Container from './Container'
-import Meta from './Meta';
+import Meta from './Meta'
 
 import './AlbumDetail.scss'
 
@@ -12,7 +12,9 @@ export default ({ data }: any) => {
   const { frontmatter, html, fields } = markdownRemark
   const { title, imageUrl, releaseYear, songs } = frontmatter
 
-  const description = `${releaseYear}년 발매. 수록곡: ${songs.map((song: string, i: number) => `${i + 1}. ${song}`).join(' ')}`
+  const description = `${releaseYear}년 발매. 수록곡: ${songs
+    .map((song: string, i: number) => `${i + 1}. ${song}`)
+    .join(' ')}`
   return (
     <Layout className="AlbumDetail">
       <Meta
@@ -24,7 +26,10 @@ export default ({ data }: any) => {
       <Container>
         <Panel noBorder>
           <div className="AlbumDetail__content">
-            <div className="AlbumDetail__poster" style={{backgroundImage: `url('${imageUrl}')`}} />
+            <div
+              className="AlbumDetail__poster"
+              style={{ backgroundImage: `url('${imageUrl}')` }}
+            />
             <div className="AlbumDetail__descriptions">
               <div className="AlbumDetail__description">
                 <h1>{title}</h1>
@@ -38,12 +43,15 @@ export default ({ data }: any) => {
                 <ol>
                   {songs.map((song: string, index: number) => (
                     <li key={song}>
-                      {index+1}. {song}
+                      {index + 1}. {song}
                     </li>
                   ))}
                 </ol>
               </div>
-              <div className="AlbumDetail__description" dangerouslySetInnerHTML={{__html: html}} />
+              <div
+                className="AlbumDetail__description"
+                dangerouslySetInnerHTML={{ __html: html }}
+              />
             </div>
           </div>
         </Panel>
